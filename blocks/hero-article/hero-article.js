43c0a582af241b736d572a1498234d6bb584188b
Luca Castagnotto <l.castagnotto@reply.it>
2026-03-09 19:19:16 +0100

    Style rules fix

--- blocks/hero-article/hero-article.js
+++ blocks/hero-article/hero-article.js
@@ -1,10 +1,10 @@
 export default function decorate(block) {
   const rows = [...block.children];
 
-  const date      = rows[0]?.children[1]?.textContent.trim() ?? '';
-  const type      = rows[1]?.children[1]?.textContent.trim() ?? '';
-  const title     = rows[2]?.children[1]?.textContent.trim() ?? '';
-  const picture   = rows[3]?.children[1]?.querySelector('picture');
+  const date = rows[0]?.children[1]?.textContent.trim() ?? '';
+  const type = rows[1]?.children[1]?.textContent.trim() ?? '';
+  const title = rows[2]?.children[1]?.textContent.trim() ?? '';
+  const picture = rows[3]?.children[1]?.querySelector('picture');
 
   block.textContent = '';
 
@@ -12,15 +12,15 @@ export default function decorate(block) {
   label.className = 'hero-label';
 
   const dateEl = document.createElement('span');
-  dateEl.className = 'hero-label__date';
+  dateEl.className = 'hero-label-date';
   dateEl.textContent = date;
 
   const separator = document.createElement('span');
-  separator.className = 'f-label__separator';
+  separator.className = 'f-label-separator';
   separator.innerHTML = '<i></i>';
 
   const typeEl = document.createElement('span');
-  typeEl.className = 'hero-label__type';
+  typeEl.className = 'hero-label-type';
   typeEl.textContent = type;
 
   label.append(dateEl, separator, typeEl);
